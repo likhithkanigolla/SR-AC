d4100f082a72bbf600bd0f604c4f5b419c48260e
Likhith Kanigolla <likhithkanigolla@gmail.com>
2025-07-27 02:00:56 +0530

    Refactor BACnetReader initialization to load once at startup and reuse in read_node_92

--- Base-FastAPI/main.py
+++ Base-FastAPI/main.py
@@ -8,6 +8,15 @@ app = FastAPI()
 
 CONFIG_PATH = os.path.join(os.path.dirname(__file__), '../default-sr-arc.config')
 
+# Load config and create BACnetReader once at startup
+config = load_config(CONFIG_PATH)
+node_92_info = get_node_info(config, 'node_92')
+raw_addr = config['bacnet']['local_interface']['device_address']
+device_address = raw_addr.split('/')[0] if '/' in raw_addr else raw_addr
+object_name = config['bacnet']['local_interface']['object_name']
+object_id = config['bacnet']['local_interface']['object_id']
+bacnet_reader = BACnetReader(device_address, object_name, object_id)
+
 @app.get("/read_property")
 def read_property(target_ip: str, obj_type: str, instance: int, prop: str):
     bacnet = BACnetReader("10.4.20.198/20:47809", "LivingLabs", 599)
@@ -16,21 +25,12 @@ def read_property(target_ip: str, obj_type: str, instance: int, prop: str):
 
 @app.get("/read_node_92")
 def read_node_92():
-    config = load_config(CONFIG_PATH)
-    node_info = get_node_info(config, 'node_92')
-    if not node_info:
+    if not node_92_info:
         return {"error": "node_92 not found in config"}
-    # Use BACnetReader with config values, strip subnet if present
-    raw_addr = config['bacnet']['local_interface']['device_address']
-    device_address = raw_addr.split('/')[0] if '/' in raw_addr else raw_addr
-    object_name = config['bacnet']['local_interface']['object_name']
-    object_id = config['bacnet']['local_interface']['object_id']
-    bacnet = BACnetReader(device_address, object_name, object_id)
-    # Example: Read Room Temp (sensor_1) for node_92
     sensor = config['sensors']['sensor_1']
     target_ip = config['bacnet']['dest']['device_address'].split(':')[0]
     obj_type = sensor['data_type'] if 'data_type' in sensor else 'analogInput'
-    instance = int(node_info['src_name'])
+    instance = int(node_92_info['src_name'])
     prop = 'presentValue'
-    result = bacnet.read_property(target_ip, obj_type, instance, prop)
+    result = bacnet_reader.read_property(target_ip, obj_type, instance, prop)
     return {"node_92_response": result}
